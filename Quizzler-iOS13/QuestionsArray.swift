46b1759c1c7cce5c1b954a273ce6fa860b729b82
sacujo <sacujo93@yandex.ru>
2023-11-07 01:09:08 +0300

    complete the original app

---
+++ Quizzler-iOS13/QuestionsArray.swift
@@ -0,0 +1,11 @@
+//
+//  QuestionsArray.swift
+//  Quizzler-iOS13
+//
+//  Created by Igor Guryan on 07.11.2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+let questionsArray = [Question]()
